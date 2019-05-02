4859d9fd8c47a917c69bcf4d6f10a2e7ed32c39c
Duong Tuan Anh <anh.duong@fruitful.io>
2019-05-02 08:28:09 +0700

    a

--- etc/project3/src/main.js
+++ etc/project3/src/main.js
@@ -1,9 +1,21 @@
 import Vue from 'vue'
-import App from './App.vue'
+import AppLayout from './components/AppLayout.vue'
+import 'babel-polyfill'
+import router from './router'
+import './global-components'
+import VueFetch from './plugins/fetch'
+import state from './state'
+import VueState from './plugins/state'
 
 new Vue({
   el: '#app',
-  render: h => {
-    return h('div','hello world');
-  }
-})
+  render: h => h(AppLayout),
+  router,
+  data: state
+});
+
+Vue.use(VueFetch, {
+  baseUrl:'http://localhost:3000/'
+});
+
+Vue.use(VueState, state);
